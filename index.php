7c47570ce9c458120db3ccb62e36a16c2078f63b
Deathkon <shyakasmith@gmail.com>
2025-01-15 21:06:05 +0200

    Integrate the components in the index.php
    Integrate the navbar and footer components in `index.php` using PHP `include` statements.
    
    * **Navbar Integration**
      - Remove the direct HTML code for the navbar.
      - Include the navbar component using `include 'components/navbar.php';` within the `<header>` tag.
    
    * **Footer Integration**
      - Remove the direct HTML code for the footer.
      - Include the footer component using `include 'components/footer.php';` within the `<footer>` tag.

--- index.php
+++ index.php
@@ -9,19 +9,7 @@
 </head>
 <body>
     <header>
-        <div class="navbar">
-            <h1>Precision Farming Hub</h1>
-            <nav>
-                <ul>
-                    <li><a href="./views/index.php">Home</a></li>
-                    <li><a href="./views/login.php">Tutorials</a></li>
-                    <li><a href="./views/sensors.php">IoT Sensors</a></li>
-                    <li><a href="./views/data.php">Data Visualizations</a></li>
-                    <li><a href="./views/quizzes.php">Quizzes</a></li>
-                    <li><a href="../views/signup.php">Signup</a></li>
-                </ul>
-            </nav>
-        </div>
+        <?php include 'components/navbar.php'; ?>
     </header>
 
     <main>
@@ -33,25 +21,7 @@
     </main>
 
     <footer>
-        <div class="footer-content">
-            <p>Your ultimate destination for IoT-powered farming knowledge and tools.</p>
-            <div class="socials">
-                <a href="https://www.linkedin.com/in/queen-clemence-01a888214?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="https://www.linkedin.com/in/queen-clemence-01a888214?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><i class="fa-brands fa-linkedin"></i></a>
-                <a href="https://www.instagram.com/umwamikazi___?igsh=dTE0ZXJtd2Ewcmhi" target="https://www.instagram.com/umwamikazi___?igsh=dTE0ZXJtd2Ewcmhi"><i class="fa-brands fa-instagram"></i></a>
-            </div>
-        </div>
-
-        <div class="footer-bottom">
-            <p>&copy; 2024 Precision Farming Hub | All Rights Reserved</p>
-            <ul class="footer-links">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="../views/login.php">Tutorials</a></li>
-                <li><a href="../views/sensors.php">IoT Sensors</a></li>
-                <li><a href="../views/data.php">Data Visualizations</a></li>
-                <li><a href="../views/quizzes.php">Quizzes</a></li>
-                <li><a href="../views/signup.php">Signup</a></li>
-            </ul>
-        </div>
+        <?php include 'components/footer.php'; ?>
     </footer>
     
 </body>
